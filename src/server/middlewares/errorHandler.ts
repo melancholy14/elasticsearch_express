52b7c983121d53c0da819a9df081a4b5dd9a3e85
Misol Goh <melancholy8914@gmail.com>
2021-09-29 23:13:19 +0200

    Update errorHandler

--- src/server/middlewares/errorHandler.ts
+++ src/server/middlewares/errorHandler.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-import HttpException from '../common/http-exception';
+import HttpException from "../common/http-exception";
 
 export const errorHandler = (
-    error: HttpException,
-    _request: Request,
-    response: Response,
+  error: HttpException,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
 ) => {
-    const status = error.statusCode || 500;
-    const message = error.message || 'Something went wrong!';
+  const status = error.statusCode || 500;
+  const message = error.message || "Something went wrong!";
 
-    response.status(status).send(message);
-}
+  response.status(status).json({
+    success: false,
+    error: message,
+  });
+};
